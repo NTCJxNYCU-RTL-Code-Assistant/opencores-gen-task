3c3206096179c966a0fd959a41fc945e9e636e88
ericlincs13 <ericlin.cs13@nycu.edu.tw>
2025-10-03 12:49:26 +0800

    finish parsing 280 pdfs

--- parse-pdf/main.py
+++ parse-pdf/main.py
@@ -1,7 +1,7 @@
 import os
 
 
-def load_project_names():
+def load_tb_project_names():
     projects_names: list[str] = []
     with open("gen-stimuli/opencores_tb_report.md", "r", encoding="utf-8") as f:
         lines = f.readlines()
@@ -41,6 +41,12 @@ def load_project_names():
     return projects_names
 
 
+def find_path_with(path, word_list: str):
+    # 取得所有父資料夾名稱
+    folders = [p.lower() for p in os.path.normpath(path).split(os.sep)]
+    return any(word in folders for word in word_list)
+
+
 def find_pdf_path(project_name: str):
     # 遞迴遍歷 dir_path 下的所有檔案
     dir_path = f"../opencores_downloads/{project_name}/{project_name}"
@@ -49,16 +55,54 @@ def find_pdf_path(project_name: str):
         for file in files:
             if file.endswith(".pdf"):
                 file_path_list.append(os.path.join(root, file))
-
-    def is_in_doc_folder(path, word_list: str):
-        # 取得所有父資料夾名稱
-        folders = [p.lower() for p in os.path.normpath(path).split(os.sep)]
-        return any(word in folders for word in word_list)
-
-    file_path_list.sort(key=lambda x: not is_in_doc_folder(x, ["doc", "docs"]))
-    file_path_list.sort(key=lambda x: not is_in_doc_folder(x, ["trunk"]))
+    file_path_list.sort(key=lambda x: not find_path_with(x, ["doc", "docs"]))
+    file_path_list.sort(key=lambda x: not find_path_with(x, ["trunk"]))
 
     return file_path_list
+
+
+# all_project_names = os.listdir("../opencores_downloads")
+# project_names = load_tb_project_names()
+
+# project_spec_types: dict[str, set[str]] = {}
+# for project_name in project_names:
+#     dir_path = f"../opencores_downloads/{project_name}/{project_name}"
+#     project_spec_type: set[str] = set()
+#     for root, dirs, files in os.walk(dir_path):
+#         for file in files:
+#             if file.endswith(".pdf"):
+#                 project_spec_type.add("pdf")
+#             elif file.endswith(".md"):
+#                 project_spec_type.add("md")
+#             elif file.endswith(".doc") or file.endswith(".docx"):
+#                 project_spec_type.add("doc")
+#             elif "spec" in file.lower():
+#                 project_spec_type.add("SPEC_FILE")
+#             elif "readme" in file.lower():
+#                 project_spec_type.add("README_FILE")
+#     if len(project_spec_type) > 0:
+#         project_spec_types[project_name] = project_spec_type
+
+# print(f"Total projects: {len(all_project_names)}")
+# print(f"Testbench projects: {len(project_names)}")
+# print(f"Testbench projects with spec: {len(project_spec_types)}")
+# print(
+#     f"Testbench projects with pdf spec: {len([project_name for project_name, spec_type in project_spec_types.items() if 'pdf' in spec_type])}"
+# )
+# print(
+#     f"Testbench projects with md spec: {len([project_name for project_name, spec_type in project_spec_types.items() if 'md' in spec_type])}"
+# )
+# print(
+#     f"Testbench projects with doc spec: {len([project_name for project_name, spec_type in project_spec_types.items() if 'doc' in spec_type])}"
+# )
+# print(
+#     f"Testbench projects with any spec_file spec: {len([project_name for project_name, spec_type in project_spec_types.items() if 'SPEC_FILE' in spec_type])}"
+# )
+# print(
+#     f"Testbench projects with any readme_file spec: {len([project_name for project_name, spec_type in project_spec_types.items() if 'README_FILE' in spec_type])}"
+# )
+
+# exit()
 
 
 if __name__ == "__main__":
@@ -69,9 +113,16 @@ if __name__ == "__main__":
     from gpt_parser import parse_pdf
 
     os.makedirs("parsed", exist_ok=True)
-    project_names = load_project_names()
+    project_names = load_tb_project_names()
     project_pdfs: list[tuple[str, list[str]]] = []
-    project_without_pdfs: list[str] = []
+    # for idx, project_name in enumerate(project_names):
+    #     pdf_path_list = find_pdf_path(project_name)
+    #     if len(pdf_path_list) > 0:
+    #         project_pdfs.append((project_name, pdf_path_list))
+
+    # with open("project_pdfs.json", "w", encoding="utf-8") as f:
+    #     json.dump(project_pdfs, f)
+
     project_skipped = os.listdir("parsed")
     project_names = [
         project_name
@@ -79,26 +130,15 @@ if __name__ == "__main__":
         if project_name not in project_skipped
     ]
 
-    # for idx, project_name in enumerate(project_names):
-    #     pdf_path_list = find_pdf_path(project_name)
-    #     if len(pdf_path_list) > 0:
-    #         project_pdfs.append((project_name, pdf_path_list))
-    #     else:
-    #         project_without_pdfs.append(project_name)
-
-    # with open("project_pdfs.json", "w", encoding="utf-8") as f:
-    #     json.dump(project_pdfs, f)
-    # with open("project_without_pdfs.json", "w", encoding="utf-8") as f:
-    #     json.dump(project_without_pdfs, f)
-
     with open("project_pdfs.json", "r", encoding="utf-8") as f:
         project_pdfs = json.load(f)
-    with open("project_without_pdfs.json", "r", encoding="utf-8") as f:
-        project_without_pdfs = json.load(f)
+        project_pdfs = [
+            (project_name, pdf_path_list)
+            for project_name, pdf_path_list in project_pdfs
+            if project_name in project_names
+        ]
 
-    print(f"Total projects to process: {len(project_names)}")
-    print(f"Total projects with pdfs: {len(project_pdfs)}")
-    print(f"Total projects without pdfs: {len(project_without_pdfs)}")
+    print(f"Total projects to process: {len(project_pdfs)}")
 
     dotenv.load_dotenv()
     api_key = os.getenv("OPENROUTER_API_KEY")
